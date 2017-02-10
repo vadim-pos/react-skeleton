402528fa9b6ff4c7682d5097787c1da1e44cf2a2
Vadim Postanovkin <v.postanovkin@gmail.com>
2017-02-10 13:40:10 +0300

    Update with tests & folder structure

---
+++ src/tests/components/main.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import expect from 'expect';
+import TestUtils from 'react-addons-test-utils';
+
+import { Main } from '../../components/main.jsx';
+
+describe('Main', () => {
+    it('should exist', () => {
+        expect(Main).toExist();
+    });
+});
